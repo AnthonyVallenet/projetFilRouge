41931fc479d76dbbce6daef1e019ef318bd22943
Smash Codeur <47318764+FaustinGarreau@users.noreply.github.com>
2020-05-26 09:31:08 +0200

    Contact front terminer

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -6,6 +6,8 @@
         <link rel="stylesheet" href="/css/style.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://kit.fontawesome.com/726b142b0a.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+
     </head>
     <body onload="checkCookie()">
         <nav id="nav" class="close">
