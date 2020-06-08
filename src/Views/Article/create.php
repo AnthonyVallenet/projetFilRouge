87fcee0110452428aa93e8dcff828ee483b0ecf0
guerlain <gpetitmaire@edenschol.fr>
2020-06-08 15:29:38 +0200

    create_article responsive

--- src/Views/Article/create.php
+++ src/Views/Article/create.php
@@ -21,7 +21,6 @@ old("enabled") == "on" ?: $enabled = "";
       <div class="input enabled">
         <input type="checkbox" id="enabled" name="enabled" <?php echo $enabled; ?>>
         <label for="enabled">Mettre en pause l'article</label>
-        <span class="error"><?php echo error("enabled");?></span>
       </div>
       <div class="input tag" id="tag">
         <p>#Tags:</p>
@@ -41,47 +40,53 @@ old("enabled") == "on" ?: $enabled = "";
 
 
     <div class="titreDate">
-      <div class="titre input">
-        <label for="title"></label>
-        <input type="text" name="title" id="title" value="<?php echo old("title");?>" placeholder="Titre">
-        <span class="error"><?php echo error("title");?></span>
+      <div class="block">
+        <div class="titre input">
+          <label for="title"></label>
+          <input type="text" name="title" id="title" value="<?php echo old("title");?>" placeholder="Titre">
+        </div>
+        <?php echo error("title") ? '<span class="error" style="margin-top: 10px"><i class="fas fa-exclamation-circle"></i>'. error("title") .'</span>' : ""?>
       </div>
-      <div class="date input">
-        <label for="date">Date: </label>
-        <input type="date" name="date" id="date" value="<?php echo old("date");?>">
-        <span class="error"><?php echo error("date");?></span>
-      </div>
+      
+      <div class="block">
+        <div class="date input">
+          <label for="date">Date: </label>
+          <input type="date" name="date" id="date" value="<?php echo old("date");?>">
+        </div>
+        <?php echo error("date") ? '<span class="error" style="margin-top: 10px"><i class="fas fa-exclamation-circle"></i>'. error("date") .'</span>' : ""?>
     </div>
+
+      </div>
 
 
       <div class="imgDescription">
         <div class="container">
           <div  class="result" >
             <img id="output" src="https://media.discordapp.net/attachments/700677739471962192/714830061496303647/image-not-found.png?width=1055&height=703" alt="">
-            <input type="file" id="file" name="" accept="image/png, image/jpeg"/>
+            <input type="file" id="file" name="imgArticle" accept="image/png, image/jpeg"/>
             <label for="file" id="labelFile" class="fileButton"><i class="fas fa-image"></i></label>
          </div>
+         <?php echo error("imgArticle") ? '<span class="error"><i class="fas fa-exclamation-circle"></i>'. error("imgArticle") .'</span>' : ""?>
         </div>
 
 
         <div class="description">
           <label for="content"></label>
           <textarea name="content" class="input" id="content" cols="30" rows="10" placeholder="content"><?php echo old("content");?></textarea>
-          <span class="error"><?php echo error("content");?></span>
+          <?php echo error("content") ? '<span class="error" style="margin-top: 10px"><i class="fas fa-exclamation-circle"></i>'. error("content") .'</span>' : ""?>
         </div>
       </div>
       <div class="commentSend">
         <label for="comment"><span>Autoriser les commentaires</span>
           <input type="checkbox" id="comment" name="comment" <?php echo $comment; ?>>
         </label>
-        <span class="error"><?php echo error("comment");?></span>
 
         <button class="button" type="submit">send</button>
       </div>
 <!-- 
         <label for="imgArticle">Img :</label>
         <input type="file" id="imgArticle" name="imgArticle" accept="image/png, image/jpeg"/>
-        <span class="error"><?php echo error("imgArticle");?></span>
+        <span class="error" style="margin-top: 10px"><?php echo error("imgArticle");?></span>
 
         <br>
 
